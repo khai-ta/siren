63ff20184bd2d715c464d888ff7a62c95619b922
Khai Ta <khaita2006@gmail.com>
2026-04-20 02:58:26 -0400

    wire evaluation

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,1 +1,9 @@
-"""Evaluation package scaffold"""
+"""Evaluation package"""
+
+from .ragas_eval import score_investigation
+from .run_benchmark import run_benchmark
+
+__all__ = [
+	"score_investigation",
+	"run_benchmark",
+]
